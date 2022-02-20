e7be0e831188ca1dcda2a3f2cf69a6f6d8cf6e4e
Konstantin <balashov-92@mail.ru>
2022-02-21 00:23:33 +0300

    feat: Add logging

--- task_manager/main.py
+++ task_manager/main.py
@@ -18,6 +18,11 @@ from databases.postgres.repos.users import UsersRepo
 from models.messages import Message
 from models.users import User
 
+
+class FormatMessageError(Exception):
+    pass
+
+
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher(bot)
 
@@ -25,7 +30,7 @@ dp = Dispatcher(bot)
 def get_data_from_message(text: str) -> Tuple[int, str]:
     resp = text.split('//')
     if len(resp) < 2:
-        return 0, ''
+        raise FormatMessageError
     else:
         return int(resp[-1]), resp[0]
 
@@ -57,8 +62,8 @@ async def process_help_command(msg: types.Message):
 
 
 @dp.message_handler(commands=['cel'])
-async def process_help_command(msg: types.Message):
-    result = 'Choose celebrity: \n'
+async def process_help_command_2(msg: types.Message):
+    result = 'Celebrities numbers: \n'
     conn = await get_db_conn()
     cel_repo = CelebritiesRepo(conn)
     res = await cel_repo.list()
@@ -72,21 +77,27 @@ async def create_task(msg: types.Message):
         REDIS_URL, username=REDIS_USERNAME, password=REDIS_PASSWORD, db=0
     )
     conn = await get_db_conn()
-
     try:
         msgs_repo = MessagesRepo(conn)
         users_repo = UsersRepo(conn)
-
         users = await users_repo.list(tg_id=msg.from_user.id)
-        user_pk = users[0].pk
+        user = users[0]
         cel_pk, text = get_data_from_message(msg.text)
-        message = Message(title=text, user_pk=user_pk, celebrity_pk=cel_pk)
+        message = Message(title=text, user_pk=user.pk, celebrity_pk=cel_pk)
         await msgs_repo.create(message)
-        task_key = f'{user_pk}_{int(time.time())}'
-        await redis.set(task_key, json.dumps({'user_pk': user_pk, 'text': text, 'cel_pk': cel_pk}))
+        task_key = f'{user.pk}_{int(time.time())}'
+        await redis.set(task_key, json.dumps({'user_pk': user.pk, 'text': text, 'cel_pk': cel_pk}))
         logger.info(f'Task key={task_key} successfully created')
     except DbFkError:
+        await bot.send_message(msg.from_user.id, f'Celebrity with pk={cel_pk} does not exist')
         logger.debug('Error in celebrity fk key')
+
+    except FormatMessageError:
+        await bot.send_message(
+            msg.from_user.id,
+            'Error occurred in message. Use /help to see true message format '
+        )
+        logger.debug('Error in message format')
     except Exception as e:
         logger.error(e)
     finally:
@@ -95,4 +106,5 @@ async def create_task(msg: types.Message):
 
 
 if __name__ == '__main__':
+    logger.info('Start task_manager')
     executor.start_polling(dp)
